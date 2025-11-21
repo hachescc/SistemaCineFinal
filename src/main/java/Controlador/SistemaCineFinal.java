daf1ec223371d55ba70da39d7d03435ee13d8357
hachescc <hugo_ale.chavarro@uao.edu.co>
2025-11-21 14:24:34 -0500

    commit creación del código

---
+++ src/main/java/Controlador/SistemaCineFinal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Controlador;
+
+/**
+ *
+ * @author hugo alejandro chavarro y juan david villegas
+ */
+public class SistemaCineFinal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
